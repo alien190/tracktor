2e236c93e4c53e885b96b142f75dbd25b6ed2c83
alien190 <alien1900@bk.ru>
2018-08-08 13:05:43 +0700

    simplify getFragment in MainActivity

--- app/src/main/java/com/elegion/tracktor/ui/result/ResultActivity.java
+++ app/src/main/java/com/elegion/tracktor/ui/result/ResultActivity.java
@@ -10,15 +10,12 @@ import com.elegion.tracktor.common.SingleFragmentActivity;
 
 public class ResultActivity extends SingleFragmentActivity {
 
-    //    public static final String STOP_ROUTE_EVENT_KEY = "StopRouteEventKey";
-//    public static final String SCREENSHOT_KEY = "ScreenShotKey";
     public static final String ID_KEY = "IdKey";
     public static final Long ID_LIST = -1L;
 
     @Override
     protected Fragment getFragment() {
-        Bundle args = getIntent().getExtras();
-        long id = args != null ? args.getLong(ID_KEY, ID_LIST) : ID_LIST;
+        long id = getIntent().getLongExtra(ID_KEY, ID_LIST);
 
         if (id == ID_LIST) {
             return ResultFragment.newInstance();
@@ -29,10 +26,6 @@ public class ResultActivity extends SingleFragmentActivity {
 
     public static void start(Context context, long id) {
         Intent intent = new Intent(context, ResultActivity.class);
-//        Bundle bundle = new Bundle();
-//        bundle.putParcelable(STOP_ROUTE_EVENT_KEY, event);
-//        bundle.putString(SCREENSHOT_KEY, ScreenshotMaker.toBase64(bitmap));
-//        intent.putExtras(bundle);
         intent.putExtra(ID_KEY, id);
         context.startActivity(intent);
     }
