2fa0be960f3da2dc34d180c786880f27b14a479f
alien190 <alien1900@bk.ru>
2018-09-28 11:17:57 +0700

    added differentiate distance units - m and km

--- app/src/main/java/com/elegion/tracktor/utils/StringUtils.java
+++ app/src/main/java/com/elegion/tracktor/utils/StringUtils.java
@@ -13,15 +13,28 @@ import java.util.Locale;
 
 public class StringUtils {
     public static String getDistanceText(Double distance) {
-        return String.format(Locale.ENGLISH, "%.1f m", distance);
+        if (distance == null || distance < 0) {
+            distance = 0d;
+        }
+        if (distance >= 1000) {
+            distance = distance / 1000;
+            return String.format(Locale.ENGLISH, "%.1f км", distance);
+        }
+        return String.format(Locale.ENGLISH, "%.1f м", distance);
     }
 
     public static String getSpeedText(Double speed) {
+        if (speed == null || speed < 0) {
+            speed = 0d;
+        }
         speed = speed * 3.6;
         return String.format(Locale.ENGLISH, "%.1f км/ч", speed);
     }
 
     public static String getCaloriesText(Double calories) {
+        if (calories == null || calories < 0) {
+            calories = 0d;
+        }
         return String.format(Locale.ENGLISH, "%.1f Ккал", calories);
     }
 
@@ -34,14 +47,16 @@ public class StringUtils {
     }
 
     public static String getLocationDataText(List<LocationData> rawLocationData) {
-        StringBuilder resultBuilder = new StringBuilder();
-        for (LocationData locationData : rawLocationData) {
-            resultBuilder.append(locationData.timeSeconds).append(',')
-                    .append(locationData.point.latitude).append(',')
-                    .append(locationData.point.longitude).append('\n');
+        if (rawLocationData != null) {
+            StringBuilder resultBuilder = new StringBuilder();
+            for (LocationData locationData : rawLocationData) {
+                resultBuilder.append(locationData.timeSeconds).append(',')
+                        .append(locationData.point.latitude).append(',')
+                        .append(locationData.point.longitude).append('\n');
+            }
+            return resultBuilder.toString();
         }
-
-        return resultBuilder.toString();
+        return "";
     }
 
     public static String getDateText(Date date) {
